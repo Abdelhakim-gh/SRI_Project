3c84f15eed3f8f42a2cb84ccd5ae5825e296f98f
Abdelhakim-gh <abdo.elghayoubi@gmail.com>
2024-11-18 12:18:09 +0100

    update in setup

--- setup/install-xapian.sh
+++ setup/install-xapian.sh
@@ -6,16 +6,16 @@ sudo apt-get install zlib1g-dev
 sudo apt-get install g++
 sudo apt-get install uuid-dev
 
-curl -O http://oligarchy.co.uk/xapian/1.2.18/xapian-core-1.2.18.tar.xz
-curl -O http://oligarchy.co.uk/xapian/1.2.18/xapian-bindings-1.2.18.tar.xz
+curl -O https://oligarchy.co.uk/xapian/1.4.26/xapian-core-1.4.26.tar.xz
+curl -O https://oligarchy.co.uk/xapian/1.4.26/xapian-bindings-1.4.26.tar.xz
 
-unxz xapian-core-1.2.18.tar.xz
-unxz xapian-bindings-1.2.18.tar.xz
+unxz xapian-core-1.4.26.tar.xz
+unxz xapian-bindings-1.4.26.tar.xz
 
-tar xvf xapian-core-1.2.18.tar
-tar xvf xapian-bindings-1.2.18.tar
+tar xvf xapian-core-1.4.26.tar
+tar xvf xapian-bindings-1.4.26.tar
 
-cd xapian-core-1.2.18
+cd xapian-core-1.4.26
 mkdir -p $HOME/local
 ./configure --prefix=$HOME/local
 ./configure
@@ -27,7 +27,7 @@ sudo make install
 # *****************************************************
 
 cd ..
-cd xapian-bindings-1.2.18
+cd xapian-bindings-1.4.26
 sudo apt-get install python-sphinx
 ./configure --with-python --prefix=$HOME/local/
 make
